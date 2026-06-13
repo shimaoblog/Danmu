0237eb0e875f89bb03d9bf452e690a9dbad9103c
石猫 <shimaoblog666@petalmail.com>
2026-06-13 13:46:42 +0800

    Update getMsg.js

--- netlify/functions/getMsg.js
+++ netlify/functions/getMsg.js
@@ -3,7 +3,7 @@ const fetch = require('node-fetch');
 // ========= 请替换为你自己的信息 ==========
 const NETLIFY_TOKEN = "nfp_5DkLhLkRdSjaFX6xBD6YJLbcz8RHXYnb7ff5";
 const SITE_ID = "c197f2e6-3496-42a5-8e10-636d84b0c79d";
-const FORM_ID = "你的表单ID";
+const FORM_ID = "6a2cee5c5a3e5800080ceb58";
 // ======================================
 
 exports.handler = async (event, context) => {
